005f026ec91cb0f50eb6d3e013ad7972f8694a51
wchen <sdjcw13@gmail.com>
2015-11-19 12:02:59 +0800

    修复找不到对应 domain 的问题；优化性能

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var limit;
 var tracer = module.exports = function(options) {
   limit = options.limit * 1000000;
   return function(req, res, next) {
-    var d = process.domain;
+    var d = domain._stack[0];
     if (!d) {
       d = domain.create();
       d.add(req);
@@ -15,43 +15,46 @@ var tracer = module.exports = function(options) {
       d.run(next);
     }
     var start = process.hrtime();
-    d._tracer = {last: start, codeLine: getCodePoint()};
+    d._leanengineTracer = {last: start, stack: new Error().stack};
 
     onHeaders(res, function() {
       var duration = getDuration();
       if (duration > limit) {
-        console.log('tracer:', process.domain._tracer.codeLine, '->', getCodePoint(), duration / 1000000, 'ms');
+        console.log('tracer:', getCodePoint(domain._stack[0]._leanengineTracer.stack), '->', getCodePoint(new Error().stack), duration / 1000000, 'ms');
       }
     });
   };
 };
 
 tracer.pin = function() {
-  if (!process.domain) {
+  if (!domain._stack[0]) {
     return console.error('OMG...');
   }
   var duration = getDuration();
   if (duration > limit) {
-    console.log('tracer:', process.domain._tracer.codeLine, '->', getCodePoint(), duration / 1000000, 'ms');
+    console.log('tracer:', getCodePoint(domain._stack[0]._leanengineTracer.stack), '->', getCodePoint(new Error().stack), duration / 1000000, 'ms');
   }
-  process.domain._tracer = {
+  domain._stack[0]._leanengineTracer = {
     last: process.hrtime(),
-    codeLine: getCodePoint()
+    stack: new Error().stack
   };
 };
 
 var getDuration = function() {
-  var diff = process.hrtime(process.domain._tracer.last);
+  var diff = process.hrtime(domain._stack[0]._leanengineTracer.last);
   return (diff[0] * 1e9 + diff[1]);
 };
 
-var getCodePoint = function() {
-  var stack = new Error().stack;
-  var start = stack.indexOf('\n', stack.indexOf('\n', stack.indexOf('\n', 6)) + 1) + 1; // 异常堆栈第四行的开头
-  var end = stack.indexOf('\n', start + 1);
-  
-  var codeLine = stack.slice(start, end);
-  var names = codeLine.split('/');
+var getCodePoint = function(stack) {
+  var end = 5; // stack 第一行字符数
+  var start, line;
+  // 找到第一个不是 tracer 的堆栈行
+  do {
+    start = end + 1;
+    end = stack.indexOf('\n', start + 1);
+    line = stack.slice(start, end);
+  } while (line.indexOf(__filename) >= 0);
+  var names = line.split('/');
   var result = names.map(function(name) {
     return name.slice(0, 2);
   }).slice(0, -1).join('/');
